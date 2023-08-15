6e8a6bc150b54d62e04711ed89e76c3ff828b38e
Kunal <rajpal.kunal1@gmail.com>
2023-08-14 22:53:19 -0500

    Added dependencies

---
+++ fingerspelling_recognition.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import numpy as np
+import pandas as pd
+import pyarrow.parquet as pq
+import tensorflow as tf
+import json
+import mediapipe
+import matplotlib
+import matplotlib.pyplot as plt
+import random
+
+from skimage.transform import resize
+from mediapipe.framework.formats import landmark_pb2
+from tensorflow import keras
+from tensorflow.keras import layers
+from tqdm.notebook import tqdm
+from matplotlib import animation, rc
+
