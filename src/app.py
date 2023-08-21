7ea9c509b3bb754f4aaa6d087e9449181c3b48f9
Sood, Abhinav <asood@obsglobal.com>
2023-08-21 18:48:04 -0500

    dkdjdhdk

--- src/app.py
+++ src/app.py
@@ -131,3 +131,67 @@ hand_images, hand_landmarks = get_hands(sample_sequence_df)
 # Fetch and show the data for right hand
 anim = create_animation(np.array(hand_images)[:, 0])
 display(HTML(anim.to_jshtml()))  # This will display the animation
+
+# Pose coordinates for hand movement.
+LPOSE = [13, 15, 17, 19, 21]
+RPOSE = [14, 16, 18, 20, 22]
+POSE = LPOSE + RPOSE
+
+X = [f'x_right_hand_{i}' for i in range(21)] + [f'x_left_hand_{i}' for i in range(21)] + [f'x_pose_{i}' for i in POSE]
+Y = [f'y_right_hand_{i}' for i in range(21)] + [f'y_left_hand_{i}' for i in range(21)] + [f'y_pose_{i}' for i in POSE]
+Z = [f'z_right_hand_{i}' for i in range(21)] + [f'z_left_hand_{i}' for i in range(21)] + [f'z_pose_{i}' for i in POSE]
+
+FEATURE_COLUMNS = X + Y + Z
+
+X_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "x_" in col]
+Y_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "y_" in col]
+Z_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "z_" in col]
+
+RHAND_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "right" in col]
+LHAND_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "left" in col]
+RPOSE_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "pose" in col and int(col[-2:]) in RPOSE]
+LPOSE_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "pose" in col and int(col[-2:]) in LPOSE]
+
+# Set length of frames to 128
+FRAME_LEN = 128
+
+# Create directory to store the new data
+if not os.path.isdir("preprocessed"):
+    os.mkdir("preprocessed")
+else:
+    shutil.rmtree("preprocessed")
+    os.mkdir("preprocessed")
+
+# Loop through each file_id
+for file_id in tqdm(dataset_df.file_id.unique()):
+    # Parquet file name
+    pq_file = f"/kaggle/input/asl-fingerspelling/train_landmarks/{file_id}.parquet"
+    # Filter train.csv and fetch entries only for the relevant file_id
+    file_df = dataset_df.loc[dataset_df["file_id"] == file_id]
+    # Fetch the parquet file
+    parquet_df = pq.read_table(f"/kaggle/input/asl-fingerspelling/train_landmarks/{str(file_id)}.parquet",
+                              columns=['sequence_id'] + FEATURE_COLUMNS).to_pandas()
+    # File name for the updated data
+    tf_file = f"preprocessed/{file_id}.tfrecord"
+    parquet_numpy = parquet_df.to_numpy()
+    # Initialize the pointer to write the output of 
+    # each `for loop` below as a sequence into the file.
+    with tf.io.TFRecordWriter(tf_file) as file_writer:
+        # Loop through each sequence in file.
+        for seq_id, phrase in zip(file_df.sequence_id, file_df.phrase):
+            # Fetch sequence data
+            frames = parquet_numpy[parquet_df.index == seq_id]
+            # Calculate the number of NaN values in each hand landmark
+            r_nonan = np.sum(np.sum(np.isnan(frames[:, RHAND_IDX]), axis = 1) == 0)
+            l_nonan = np.sum(np.sum(np.isnan(frames[:, LHAND_IDX]), axis = 1) == 0)
+            no_nan = max(r_nonan, l_nonan)
+            
+            if 2*len(phrase)<no_nan:
+                features = {FEATURE_COLUMNS[i]: tf.train.Feature(
+                    float_list=tf.train.FloatList(value=frames[:, i])) for i in range(len(FEATURE_COLUMNS))}
+                features["phrase"] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(phrase, 'utf-8')]))
+                record_bytes = tf.train.Example(features=tf.train.Features(feature=features)).SerializeToString()
+                file_writer.write(record_bytes)
+                
+tf_records = dataset_df.file_id.map(lambda x: f'/kaggle/working/preprocessed/{x}.tfrecord').unique()
+print(f"List of {len(tf_records)} TFRecord files.")
